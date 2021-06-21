173bdc90028d30f2ad915a77069657058bdde45f
Pedro Farias <pedronutrinorte@gmail.com>
2021-06-21 14:24:29 -0400

    Janela Cadastrar Produto; Cadastrar Cliente; Cadastrar Funcionário

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,21 +25,41 @@ namespace PETTWARE
             InitializeComponent();
         }
 
-        private void Click_Produto(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void Click_CadServico(object sender, RoutedEventArgs e)
         {
             CadastrarServicoWindow CadServico = new CadastrarServicoWindow();
             CadServico.ShowDialog();
         }
 
+        private void Click_Produto(object sender, RoutedEventArgs e)
+        {
+            CadProdutoWindow Produto = new CadProdutoWindow();
+            Produto.ShowDialog();
+        }
+
+        private void Click_CadCliente(object sender, RoutedEventArgs e)
+        {
+            CadClienteWindow1 Cliente = new CadClienteWindow1();
+            Cliente.ShowDialog();
+        }
+
+        private void Click_CadFuncionario(object sender, RoutedEventArgs e)
+        {
+            CadFuncionarioWindow1 Funcionario = new CadFuncionarioWindow1();
+            Funcionario.ShowDialog();
+        }
+
+
+
         private void Click_Vender(object sender, RoutedEventArgs e)
         {
             FazerVendaWindow Vender = new FazerVendaWindow();
             Vender.ShowDialog();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
