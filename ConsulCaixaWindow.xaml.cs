b8114d807c0e536ac2010f7ea7c324591e1d8e34
alinne-art <alinnetonettiiiii@gmail.com>
2021-06-24 17:45:20 -0400

    Cadastrar tipos de despesas e Consultar caixa

---
+++ ConsulCaixaWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PETTWARE
+{
+    /// <summary>
+    /// Lógica interna para ConsulCaixaWindow.xaml
+    /// </summary>
+    public partial class ConsulCaixaWindow : Window
+    {
+        public ConsulCaixaWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
