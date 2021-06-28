e54c5582344a70f8ad6ce07dbf427cb122e6d748
Caian Henrique <caianhenrique10@gmail.com>
2021-06-28 17:44:15 -0400

    Fazer Venda

---
+++ Venda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PETTWARE
+{
+    class Venda
+    {
+        public int ID { get; set; }
+
+        public string Produto { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public string ValorUnitario { get; set; }
+
+        public string ValorcomDesconto { get; set; }
+    }
+}
