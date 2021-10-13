002a6b1f7e5731a550bec8c6113f46783d318e06
Milena Lucas <milenalucas2003@gmail.com>
2021-10-13 15:52:13 -0300

    atu fornecedor e despesas - DAO

--- Models/DespesasDAO.cs
+++ Models/DespesasDAO.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PETTWARE.Interfaces;
 
 namespace PETTWARE.Models
 {
-    class DespesasDAO
+    class DespesasDAO : IDAO<Despesas>
     {
+        public void Delete(Despesas t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Despesas GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Despesas t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Despesas> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Despesas t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Models/FonecedorDAO.cs
+++ Models/FonecedorDAO.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PETTWARE.Interfaces;
+
 
 namespace PETTWARE.Models
 {
-    class FonecedorDAO
+    class FonecedorDAO : IDAO<Fornecedor>
     {
+        public void Delete(Fornecedor t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fornecedor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Fornecedor t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fornecedor> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Fornecedor t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
