12d5ff7bd4d46e6bfbfd7fee661bb804c4190a0d
Caian Henrique <caianhenrique10@gmail.com>
2021-10-13 18:00:23 -0400

    Atualização

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -8,6 +8,11 @@ namespace PETTWARE
     {
         public int ID { get; set; }
 
+        public int codCli { get; set; }
+
+        public int codFunc { get; set; }
+
+        public string data { get; set; }
         public string Produto { get; set; }
 
         public int Quantidade { get; set; }
