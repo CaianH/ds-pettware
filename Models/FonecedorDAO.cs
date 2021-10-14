d4da3c19ff86ac067b5275ab5217e3379bfea392
Milena Lucas <milenalucas2003@gmail.com>
2021-10-14 17:40:47 -0300

    atualização

--- Models/FonecedorDAO.cs
+++ Models/FonecedorDAO.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using PETTWARE.Interfaces;
+using PETTWARE.DataBase;
+
 
 
 namespace PETTWARE.Models
 {
     class FonecedorDAO : IDAO<Fornecedor>
     {
+
+        private static conexao conn;
+
+        public FonecedorDAO()
+        {
+            conn = new conexao();
+        }
+
+        
         public void Delete(Fornecedor t)
         {
             throw new NotImplementedException();
@@ -20,7 +31,29 @@ namespace PETTWARE.Models
 
         public void Insert(Fornecedor t)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var query = conn.Query();
+                query.CommandText = "INSERT INTO fornecedor (cnpj_forn, nome_forn, telefone_forn, email, representante_forn) " +
+                    "VALUES  (@cnpj, @nome, @telefone, @email, @representante)";
+
+                query.Parameters.AddWithValue("@cnpj", t.CNPJ);
+                query.Parameters.AddWithValue("@nome", t.Nome);
+                query.Parameters.AddWithValue("@telefone", t.Telefone);
+                query.Parameters.AddWithValue("@email", t.Email);
+                query.Parameters.AddWithValue("@representante", t.Representante);
+
+                var result = query.ExecuteNonQuery();
+
+
+
+            } catch (Exception e)
+            {
+
+            } finally
+            {
+                conn.Close();
+            }
         }
 
         public List<Fornecedor> List()
