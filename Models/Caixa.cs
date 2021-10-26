b68b34ee13675b15dbb50c58622da46299c1e5a3
alinne-art <alinnetonettiiiii@gmail.com>
2021-10-26 02:23:30 -0400

    atualização caixa

---
+++ Models/Caixa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PETTWARE.Models
+{
+    class Caixa
+    {
+        public int cod { get; set; }
+        public double receitatotal { get; set; }
+        public double despesatotal { get; set; }
+        public double lucro { get; set; }
+        public double debito { get; set; }
+        
+    }
+}
---
+++ Models/CaixaDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PETTWARE.Interfaces;
+using PETTWARE.DataBase;
+
+
+namespace PETTWARE.Models
+{
+    class CaixaDAO
+    {
+        private static conexao conn;
+
+        public CaixaDAO()
+        {
+            conn = new conexao();
+        }
+
+        public void Insert(Caixa t)
+        {
+            try
+            {
+                var query = conn.Query();
+                query.CommandText = "INSERT INTO caixa (receitatotal_caixa, despesatotal_caixa, lucro_caixa, debito_caixa )" +
+                    "VALUES ( @receitatotal, @despesatotal, @lucro, @debito)";
+
+
+                query.Parameters.AddWithValue("@receitatotal", t.receitatotal);
+                query.Parameters.AddWithValue("@despesatotal", t.despesatotal);
+                query.Parameters.AddWithValue("@lucro", t.lucro);
+                query.Parameters.AddWithValue("@debito", t.debito);
+              
+
+                var result = query.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+    }
+}
